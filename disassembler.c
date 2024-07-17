aab3f2950e33a407a8754876fde544ae40b159d7
njfletcher <njfletcher21@gmail.com>
2024-07-16 23:53:40 -0400

    start

---
+++ disassembler.c
@@ -0,0 +1,42 @@
+
+
+
+
+
+//assumes this is given a 15 byte string. a legal instruction might only make up part of this string, or all of it.
+prefixFSM(char* instructionCandidate){
+	
+	//cant be more than 5
+	int numPrefixSeen = 0;
+        //	
+
+	//opSize none 0x66
+	//addrSize none 0x67
+	// segCS: CS 0x2E 
+	//segDS: DS 0x3E
+	//segES: ES 0x26
+	//segFS: FS 0x64
+	//segGS: GS 0x65
+	//segSS: SS 0x36
+	//lock: lock 0xF0
+	//rep: REP 0xF3
+	//repez: repe/repz 0xF3
+	//repnez: repne/repnz 0xF2
+
+
+}
+
+int main(int argc, char** argv){
+
+
+
+
+
+
+
+
+
+
+
+
+}
