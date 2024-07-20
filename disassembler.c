64e02da2f539cf1580746dc90abfa31e580beea0
njfletcher <njfletcher21@gmail.com>
2024-07-19 22:10:55 -0400

    started secondary prefixes and opcode maps

--- disassembler.c
+++ disassembler.c
@@ -14,11 +14,11 @@
 #define repezPre = 0xF3; //repe or repz
 #define repnezPre = 0xF2; // repne or repnz
 
-//opSizePre, addrSizePre, segmentPre(0/1-6), repPre(0/1-3)
+//opSizePre, addrSizePre, segmentPre(0/1-6),lockPre, repPre(0/1-3)
 char legacyPrefixInfo[5] = {0,0,0,0,0};
 
 //assumes this is given a 15 byte string. a legal instruction might only make up part of this string, or all of it.
-int legacyPrefixFSM(char* instructionCandidate){
+char legacyPrefixFSM(char* instructionCandidate){
 
 	char bytesRead = 0;
 	char* currentPos = instructionCandidate;
@@ -61,17 +61,22 @@ int legacyPrefixFSM(char* instructionCandidate){
 				morePrefixesPossible = !legacyPrefixInfo[2];
 				legacyPrefixInfo[2] = 6;
 				break;
-			case repPre:
-				morePrefixesPossible = !legacyPrefix[3];
+			case lockPre:
+				//lock is mutually exclusive with repeaters
+				morePrefixesPossible = !legacyPrefixInfo[3] || !legacyPrefixInfo[4];
 				legacyPrefixInfo[3] = 1;
 				break;
+			case repPre:
+				morePrefixesPossible = !legacyPrefix[4] || !legacyPrefixInfo[3];
+				legacyPrefixInfo[4] = 1;
+				break;
 			case repezPre:
-				morePrefixesPossible = !legacyPrefix[3];
-				legacyPrefixInfo[3] = 2;
+				morePrefixesPossible = !legacyPrefix[4] || !legacyPrefixInfo[3];
+				legacyPrefixInfo[4] = 2;
 				break;
 			case repenzPre:
-				morePrefixesPossible = !legacyPrefix[3];
-				legacyPrefixInfo[3] = 3;
+				morePrefixesPossible = !legacyPrefix[4] || !legacyPrefixInfo[3];
+				legacyPrefixInfo[4] = 3;
 				break;
 			case default:
 				//unrecognized legacy prefix
@@ -92,9 +97,119 @@ int legacyPrefixFSM(char* instructionCandidate){
 
 }
 
+char isRexPrefix(char byte){
+
+	return ((byte & 0xF0) == 0x40);
+
+
+}
+
+
+//rex prefix, 0F escape, vex/xop prefix
+char secondaryPrefixInfo[3] = {0,0,0};
+
+//assumes this is given a 15 byte string. a legal instruction might only make up part of this string, or all of it.
+char secondaryPrefixFSM(char * currentByte){
+
+	//rex prefix ranges from 40 to 4F
+	
+	//rex prefix + primary opcode
+	//rex prefix + 0F escape
+	// no rex prefix + primary opcode
+	//no rex prefix + 0F escape
+	//no rex prefix + vex prefix
+	// no rex prefix + xop prefix
+	
+	char currChar = *instructionCandidate;
+	if(isRexPrefix(currChar)){
+	
+		secondaryPrefixInfo[0] = 1;
+		currChar = *(instructionCandidate + 1)
+		if(currChar == 0x0F)secondaryPrefixInfo[1] = 1;
+		return 0;
+	
+	}
+	else{
+		switch(currChar){
+		
+			case 0xC5:
+			//vex 2 byte sequence
+			
+			case 0xC4:
+			//vex 3 byte sequence
+			
+			
+			case 0x8F:
+			//xop 3 byte sequence
+			
+			case default:
+			
+			break;
+			
+		
+		
+		
+		
+		
+		}
+	
+	
+	
+	}
+
+
+
+
+
+
+
+}
+
+
+int mapSelect(char mapCode){
+
+	switch(mapCode){
+	
+		case 0:
+		//primary opcode
+		break;
+		case 1:
+		//second opcode
+		break;
+		case 2:
+		//0F38 opcode
+		break;
+		case 3:
+		//0F3A opcode
+		case 4:
+		//vex 1 opcode
+		break;
+		case 5:
+		//vex 2 opcode
+		break;
+		case 6:
+		//vex 3 opcode
+		break;
+		case 7:
+		//xop 8 opcode
+		break;
+		case 8:
+		//xop 9 opcode
+		break;
+		case 9:
+		//xop A opcode
+		case default:
+			return -1;
+	
+	}
+
+
+}
+
 int main(int argc, char** argv){
 
-
+	char instruction[15];
+	legacyPrefixFSM(instruction);
 
 
 
